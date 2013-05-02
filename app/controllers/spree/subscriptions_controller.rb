5704d571c28bcdea8fc0ad84ae4d751d337c5e83
Roman Laukhin <r.laukhin@gmail.com>
2013-05-02 17:59:46 +0300

    some changes for compatability with spree 1-3-stable

--- app/controllers/spree/subscriptions_controller.rb
+++ app/controllers/spree/subscriptions_controller.rb
@@ -1,7 +1,7 @@
 class Spree::SubscriptionsController < Spree::BaseController
 
   def hominid
-    @hominid ||= Hominid::API.new(Spree::Config.get(:mailchimp_api_key))
+    @hominid ||= Hominid::API.new(Spree::Config.get(:mailchimp_api_key),{:timeout => 60})
   end
 
   def create
