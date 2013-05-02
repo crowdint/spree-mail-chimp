49ba49decb1a144af8ca8524ee0aa7d70aa386c2
Roman Laukhin <r.laukhin@gmail.com>
2013-05-02 18:50:54 +0300

    fixed admin sidebar menu

--- app/overrides/add_mail_chimp_admin_menu_link.rb
+++ app/overrides/add_mail_chimp_admin_menu_link.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path  => 'spree/admin/configurations/index',
+Deface::Override.new(:virtual_path  => 'spree/admin/shared/_configuration_menu',
                      :name          => 'add_mail_chimp_admin_menu_link',
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'],#admin_configurations_sidebar_menu[data-hook]",
                      :partial       => 'spree/admin/configurations/spree_mail_chimp_configuration_link' )
